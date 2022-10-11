98808ee552b0137a3b2b144163e8c0e461f579c0
Vitorir <vitorireboucas@edu.unifor.br>
2022-10-11 18:42:12 -0300

    changes to playlist-bg

--- src/pages/Playlists/PlaylistDetail.js
+++ src/pages/Playlists/PlaylistDetail.js
@@ -20,9 +20,72 @@ function PlaylistDetail() {
 
     return (
         <>
-        <div>{play.nome}</div>
-        <img src={play.capa}></img>
-        <div>{musicas}</div>
+    <div className="container-fluid h-100">
+      <div className="row justify-content-center h-100">
+        <div className="col-2 bg-black text-white">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a className="nav-link" href="index.html">
+                <img
+                  src="/img/home2.png"
+                  width="25"
+                  height="25"
+                  alt=""
+                />{" "}
+                Início
+                <br />
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="">
+                <img
+                  src="/img/search.png"
+                  alt=""
+                  width="25"
+                  height="25"
+                />{" "}
+                Buscar
+                <br />
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="">
+                <img src="/img/books.png" alt="" width="25" height="25" />
+                Sua Biblioteca <br />
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="">
+                <img
+                  src="/img/playlist.png"
+                  alt=""
+                  width="25"
+                  height="25"
+                />{" "}
+                Criar Playlist
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="col-10 bg-dark text-white">
+          <div className="">
+            <div id="">
+              <h4>{play.nome}</h4>
+              <div className="container">
+                <br />
+                <div className="row">
+                    <img src={play.capa} alt="" width={50} height={500}></img>
+                    <div>{musicas}</div>
+                  
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+
         </>
     )
 }
